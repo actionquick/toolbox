eeec5262d515fd5305d0d2f4fe3fbf9343ffd73d
actionquick <actionquick@outlook.com>
2021-11-18 17:30:16 +0800

    W-1 add config and mian files add ui src for home window.

---
+++ toolbox_main.py
@@ -0,0 +1,10 @@
+import sys
+import uisrc.home_page
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
+if __name__ == '__main__':
+  myapp = QApplication(sys.argv)
+  myDlg = QMainWindow()
+  myUI = uisrc.home_page.Ui_HomeWindow()
+  myUI.setupUi(myDlg)
+  myDlg.show()
+  sys.exit(myapp.exec_())
